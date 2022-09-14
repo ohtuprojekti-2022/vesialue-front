dce2d52d7b8044b66cc0ddb8ad60b62483634046
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-09-14 17:50:20 +0300

    more links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap'
 const Navbar = () => {
 	
 	return (
-		<BNavbar bg="#eeeeee" fixed="top" className="py-3" collapseOnSelect>
+		<BNavbar bg="dark" variant="dark" fixed="top" className="py-3" collapseOnSelect>
 			<BNavbar.Brand href="/">
 				<img
 					src="/logo192.png"
@@ -13,17 +13,22 @@ const Navbar = () => {
 					height="30"
 					className="d-inline-block align-top"
 					alt="MAS logo"
-				/>
+				/>{' '}
+				Vesialueen inventointi-ilmoitus
 			</BNavbar.Brand>
 			<BNavbar.Toggle aria-controls="basic-navbar-nav" />
 			<BNavbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 				<Nav>
-					<LinkContainer to="/home">
+					<LinkContainer to="/">
 						<Nav.Link>Etusivu</Nav.Link>
 					</LinkContainer>
-					<LinkContainer to="/login">
+					<LinkContainer to="/kirjaudu">
 						<Nav.Link>Kirjaudu</Nav.Link>
 					</LinkContainer>
+					<LinkContainer to="/rekisteröidy">
+						<Nav.Link>Rekisteröidy</Nav.Link>
+					</LinkContainer>
+						
 				</Nav>
 			</BNavbar.Collapse>
 		</BNavbar>
