ef4d013ed2012913adc51f992563ca2ec9ce9bf9
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-09-20 12:50:17 +0300

    Add some validation to registration form
    Also store user token to browser

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -8,41 +8,32 @@ const RegistrationForm = () => {
 	const [email, setEmail] = useState('')
 	const [phone, setPhone] = useState('')
 	const [name, setName] = useState('')
+	const [validated, setValidated] = useState(false)
 
 	const handleSubmit = async event => {
 		event.preventDefault()
-		registerNewUser(username, password, email, phone, name)
+		const form = event.currentTarget
+
+		if (form.checkValidity() === false) {
+			// handle errors
+		} else {
+			setValidated(true)
+			const data = await registerNewUser(username, password, email, phone, name)
+			window.localStorage.setItem('auth', data.auth)
+
+			setUsername('')
+			setPassword('')
+			setEmail('')
+			setPhone('')
+			setName('')
+			setValidated(false)
+		}
 	}
 
 	return (
 		<Container fluid="sm">
-			<Form onSubmit={handleSubmit}>
-				<FloatingLabel
-					controlId="name"
-					label="Etu- ja sukunimi"
-					className="mb-3"
-				>
-					<Form.Control
-						type="text"
-						placeholder="Etu- ja sukunimi"
-						onChange={e => setName(e.target.value)}
-					/>
-				</FloatingLabel>
-				<FloatingLabel controlId="email" label="Sähköposti" className="mb-3">
-					<Form.Control
-						type="email"
-						placeholder="Sähköposti"
-						onChange={e => setEmail(e.target.value)}
-						required
-					/>
-				</FloatingLabel>
-				<FloatingLabel controlId="phone" label="Puhelinnumero" className="mb-3">
-					<Form.Control
-						type="phone"
-						placeholder="Puhelinnumero"
-						onChange={e => setPhone(e.target.value)}
-					/>
-				</FloatingLabel>
+			<h2>Luo uusi tunnus</h2>
+			<Form noValidate validated={validated} onSubmit={handleSubmit}>
 				<FloatingLabel
 					controlId="username"
 					label="Käyttäjänimi"
@@ -51,7 +42,20 @@ const RegistrationForm = () => {
 					<Form.Control
 						type="text"
 						placeholder="Käyttäjänimi"
+						value={username}
 						onChange={e => setUsername(e.target.value)}
+						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Käyttäjänimi ei voi olla tyhjä.
+					</Form.Control.Feedback>
+				</FloatingLabel>
+				<FloatingLabel controlId="email" label="Sähköposti" className="mb-3">
+					<Form.Control
+						type="email"
+						placeholder="Sähköposti"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
 						required
 					/>
 				</FloatingLabel>
@@ -59,8 +63,32 @@ const RegistrationForm = () => {
 					<Form.Control
 						type="password"
 						placeholder="Salasana"
+						value={password}
 						onChange={e => setPassword(e.target.value)}
 						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Salasana ei voi olla tyhjä.
+					</Form.Control.Feedback>
+				</FloatingLabel>
+				<FloatingLabel
+					controlId="name"
+					label="Etu- ja sukunimi"
+					className="mb-3"
+				>
+					<Form.Control
+						type="text"
+						placeholder="Etu- ja sukunimi"
+						value={name}
+						onChange={e => setName(e.target.value)}
+					/>
+				</FloatingLabel>
+				<FloatingLabel controlId="phone" label="Puhelinnumero" className="mb-3">
+					<Form.Control
+						type="phone"
+						placeholder="Puhelinnumero"
+						value={phone}
+						onChange={e => setPhone(e.target.value)}
 					/>
 				</FloatingLabel>
 				<Button variant="primary" type="submit">
