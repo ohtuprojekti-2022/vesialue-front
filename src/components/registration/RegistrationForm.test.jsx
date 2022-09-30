456e98ece97ba04c93ec0ff28994d59060703543
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-09-30 22:54:41 +0300

    Add tests for registration form

--- src/components/registration/RegistrationForm.test.jsx
+++ src/components/registration/RegistrationForm.test.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-import RegistrationForm from './RegistrationForm'
+import userEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
 import { act } from 'react-dom/test-utils'
+import RegistrationForm from './RegistrationForm'
 
 
 describe('Registration Form', () => {
+	const mockHandleSubmit = jest.fn()
+	const mockSetUsername = jest.fn()
+	const mockSetEmail = jest.fn()
+	const mockValidated = jest.fn()
+
+	let username, email, password, submitButton
+
 	beforeEach(() => {
 		render(
 			<MemoryRouter>
-				<RegistrationForm />
+				<RegistrationForm
+					handleSubmit={mockHandleSubmit}
+					validated={mockValidated}
+					setUsername={mockSetUsername}
+					setEmail={mockSetEmail} />
 			</MemoryRouter>
 		)
+		username = screen.getByTestId('username')
+		email = screen.getByTestId('email')
+		password = screen.getByTestId('password')
+		submitButton = screen.getByTestId('submit')
+
+		mockHandleSubmit.mockImplementation(e => {
+			e.preventDefault()
+		})
 	})
 
-	test('renders registration form', () => {
+	test('renders the registration form', () => {
 		const element = screen.getByTestId('registration-form')
 		expect(element).toBeDefined()
 	})
@@ -24,12 +44,40 @@ describe('Registration Form', () => {
 		expect(localStorage.getItem('userDetails')).toBeNull()
 	})
 
-	test('registration requires username', async () => {
-		const submitButton = screen.getByTestId('submit')
+	test('username is required', async () => {
 		act(() => {
 			submitButton.click()
 		})
-		const element = screen.getByText('Anna vähintään 3 merkkiä pitkä käyttäjänimi!')
-		expect(element).toBeDefined()
+		expect(username).toBeRequired()
+		expect(username).toBeInvalid()
+		expect(screen.getByText('Anna vähintään 3 merkkiä pitkä käyttäjänimi!')).toBeVisible()
+	})
+
+	test('password is required', async () => {
+		act(() => {
+			submitButton.click()
+		})
+		expect(password).toBeRequired()
+		expect(password).toBeInvalid()
+		expect(screen.getByText('Anna vähintään 10 merkkiä pitkä salasana!')).toBeVisible()
+	})
+
+	test('email address is required', async () => {
+		act(() => {
+			submitButton.click()
+		})
+		expect(email).toBeRequired()
+		expect(email).toBeInvalid()
+		expect(screen.getByText('Anna validi sähköpostiosoite!')).toBeVisible()
+	})
+
+	test('email address must be in the correct format', async () => {
+		const user = userEvent.setup()
+		await user.type(email, 'invalid.mail.com')
+
+		act(() => {
+			submitButton.click()
+		})
+		expect(email).toBeInvalid()
 	})
 })
