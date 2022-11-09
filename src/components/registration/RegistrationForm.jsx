ae6fdc422468aceb84e805fd3b5ebbe700acd5d3
lukxsx <33071953+lukxsx@users.noreply.github.com>
2022-11-09 13:22:30 +0200

    New phone number checking regex
    Allow + in the beginning. Allow spaces and - as separators. Set maximum length and minimum length

--- src/components/registration/RegistrationForm.jsx
+++ src/components/registration/RegistrationForm.jsx
@@ -61,7 +61,7 @@ const RegistrationForm = ({
 					type="phone"
 					placeholder="Puhelinnumero"
 					onChange={(e) => setPhone(e.target.value)}
-					pattern="^((04[0-9]{1})(\s?|-?)|050(\s?|-?)|0457(\s?|-?)|[+]?358(\s?|-?)50|0358(\s?|-?)50|00358(\s?|-?)50|[+]?358(\s?|-?)4[0-9]{1}|0358(\s?|-?)4[0-9]{1}|00358(\s?|-?)4[0-9]{1})(\s?|-?)(([0-9]{3,4})(\s|-)?[0-9]{1,4})$"
+					pattern="^\+?(?:[0-9][ |-]?){6,14}[0-9]$"
 				/>
 				<Form.Control.Feedback type="invalid">
 					Anna suomalainen puhelinnumero!
