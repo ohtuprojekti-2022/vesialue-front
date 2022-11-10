e720414cf8e605233ef8203b50bd59c8319a2171
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-11-10 13:43:29 +0200

    finish navbar tests

--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -4,14 +4,18 @@ import '@testing-library/jest-dom'
 import Navbar from './Navbar'
 import { MemoryRouter } from 'react-router-dom'
 import { renderWithProviders } from '../utils/test-tools'
+import store from '../redux/store'
+import { login } from '../redux/reducers/userReducer'
+import userEvent from '@testing-library/user-event'
 
+const userDetails = {'auth':'xyz',
+					'user':{'id':'ifbr2sa3mxdqrzgjo6nmw862', 'name':'Mikko',
+					'email':'mikko@email.fi', 'phone':'0404040400',
+					'username':'mikko1', 'admin':'0'}}
+
+store.dispatch(login(userDetails))
 
 describe('Navbar', () => {
-
-	// const userDetails = {'auth':'xyz',
-	//	'user':{'id':'ifbr2sa3mxdqrzgjo6nmw862', 'name':'Mikko',
-	//		'email':'mikko@email.fi', 'phone':'0404040400',
-	//		'username':'mikko1', 'admin':'0'}}
     
 	beforeEach(() => {
 		renderWithProviders(
@@ -19,10 +23,49 @@ describe('Navbar', () => {
 				<Navbar />
 			</MemoryRouter>
 		)
+		
 	})
 
-	test('shows user as Käyttäjä when logged out', () => {
+	test('shows username when logged in', () => {
+		expect(screen.getByText('mikko1')).not.toBeNull
+	})
+
+	test('shows logout button when logged in', async () => {
+		const DropDown = screen.getByRole('button', { name: /mikko1/i })
+		const user = userEvent.setup()
+		await user.click(DropDown)
+		expect(screen.getByText('Kirjaudu ulos')).not.toBeNull
+	})
+
+	test('shows own page button when logged in', async () => {
+		const DropDown = screen.getByRole('button', { name: /mikko1/i })
+		const user = userEvent.setup()
+		await user.click(DropDown)
+		expect(screen.getByText('Oma sivu')).not.toBeNull
+	})
+
+	test('shows user as Käyttäjä when logged out', async () => {
+		const DropDown = screen.getByRole('button', { name: /mikko1/i })
+		const user = userEvent.setup()
+		await user.click(DropDown)
+		const logoutButton = screen.getByText('Kirjaudu ulos')
+		await user.click(logoutButton)
 		expect(screen.getByText('Käyttäjä')).not.toBeNull
+	})
+	// after this test the user stays logged out
+
+	test('shows login when logged out', async () => {
+		const user = userEvent.setup()
+		const DropDown2 = screen.getByRole('button', { name: /Käyttäjä/i })
+		await user.click(DropDown2)
+		expect(screen.getByText('Kirjaudu')).not.toBeNull
+	})
+
+	test('shows register when logged out', async () => {
+		const user = userEvent.setup()
+		const DropDown2 = screen.getByRole('button', { name: /Käyttäjä/i })
+		await user.click(DropDown2)
+		expect(screen.getByText('Rekisteröidy')).not.toBeNull
 	})
 
 })
