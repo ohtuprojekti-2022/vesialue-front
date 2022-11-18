8fb45689eeeb32775c4ebd3bed55239810746644
Natalie Johansson <mnrjohansson@gmail.com>
2022-11-18 14:35:31 +0200

    simplify creator rendering in edited list item

--- src/components/inventory/EditedInventoryItem.jsx
+++ src/components/inventory/EditedInventoryItem.jsx
@@ -3,14 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { parseCreator, formatDate } from '../../utils/tools'
 
 const EditedInventoryListItem = ({ report, columns }) => {
-	const name = parseCreator(report)
 	const navigate = useNavigate()
 	return(
 		<tr onClick={() => navigate(`/muokatut/${report.id}`)}>
 			{columns.date && <td>{formatDate(report.inventorydate)}</td>}
-			{columns.creator && (name !== '' && (<td>{name}</td>)) || name === '' && (
-				<td>Tuntematon</td>
-			)}
+			<td>{parseCreator(report)}</td>
 			{columns.reason && <td>{report.editReason}</td>}
 		</tr>
 	)
