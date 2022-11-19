4a244a39d402e427325eae69051077cbee1e8267
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-11-19 19:02:18 +0200

    add some tests for userpage

--- src/components/user/UserPage.test.jsx
+++ src/components/user/UserPage.test.jsx
@@ -27,25 +27,48 @@ describe('UserPage', () => {
 	})
 
 	test('renders user details', () => {
-		expect(screen.getByText('Käyttäjätiedot')).not.toBeNull
+		expect(screen.getByText('Käyttäjätiedot')).not.toBeNull()
 	})
 
 	test('renders own inventories', () => {
-		expect(screen.getByText('Omat inventoinnit')).not.toBeNull
+		expect(screen.getByText('Omat inventoinnit')).not.toBeNull()
 	})
 
-	test('opens useredit form when clicked', async () => {
+	test('opens user details when clicked', async () => {
 		const button = screen.getByText('Käyttäjätiedot')
 		const user = userEvent.setup()
 		await user.click(button)
-		expect(screen.getByText('Käyttäjänimi')).not.toBeNull
+		expect(screen.getByText('Käyttäjänimi')).not.toBeNull()
 	})
 
 	test('opens own inventories when clicked', async () => {
 		const button = screen.getByText('Omat inventoinnit')
 		const user = userEvent.setup()
 		await user.click(button)
-		expect(screen.getByText('Inventoinnin päivämäärä')).not.toBeNull
+		expect(screen.getByText('Inventoinnin päivämäärä')).not.toBeNull()
+	})
+
+	test('edit button enables text fields', async () => {
+		const button1 = screen.getByText('Käyttäjätiedot')
+		const user = userEvent.setup()
+		await user.click(button1)
+		const button2 = screen.getByText('Muokkaa')
+		await user.click(button2)
+		expect(screen.getByLabelText('Käyttäjänimi')).not.toBeDisabled()
+		expect(screen.getByLabelText('Nimi')).not.toBeDisabled()
+		expect(screen.getByLabelText('Sähköposti')).not.toBeDisabled()
+		expect(screen.getByLabelText('Puhelinnumero')).not.toBeDisabled()
+	})
+
+	test('change password button opens password edit modal', async () => {
+		const button1 = screen.getByText('Käyttäjätiedot')
+		const user = userEvent.setup()
+		await user.click(button1)
+		const button2 = screen.getByText('Vaihda salasana')
+		await user.click(button2)
+		expect(screen.getByText('Nykyinen salasana')).not.toBeNull()
+		expect(screen.getByText('Uusi salasana')).not.toBeNull()
+		expect(screen.getByText('Salasana uudestaan')).not.toBeNull()
 	})
 
 })
