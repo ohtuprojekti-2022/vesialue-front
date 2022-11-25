02c4c43e2d568680e7a7a85ed9dcb60a2d62cdc9
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-11-25 13:37:05 +0200

    Highlight area on mouse hover

--- src/components/map/Area.jsx
+++ src/components/map/Area.jsx
@@ -5,10 +5,31 @@ import InventoryPopup from './InventoryPopup'
 
 const Area = ({ details, coordinates }) => {
 	const positionList = coordinates.map((c) => [c.lat, c.lng])
+	const defaultStyle = {
+		fillColor: '',
+		fillOpacity: 0.2
+	}
+
+	const highlightArea = (e) => {
+		const layer = e.target
+		layer.setStyle({
+			fillColor: '#CA6969',
+			fillOpacity: 0.4
+		})
+	}
+	const resetStyle = (e) => {
+		const layer = e.target
+		layer.setStyle(defaultStyle)
+	}
 
 	return (
 		<FeatureGroup>
-			<Polygon positions={positionList} />
+			<Polygon
+				positions={positionList}
+				eventHandlers={{
+					mouseover: (e) => highlightArea(e),
+					mouseout: (e) => resetStyle(e)
+				}} />
 			{details && <InventoryPopup details={details} />}
 		</FeatureGroup>
 	)
