f54947048bd31f7adebee45c1520196ef2b0a3d7
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-11-27 22:03:23 +0200

    lint is happy

--- src/components/footer.test.jsx
+++ src/components/footer.test.jsx
@@ -7,40 +7,40 @@ import { renderWithProviders } from '../utils/test-tools'
 import userEvent from '@testing-library/user-event'
 
 describe('Footer', () => {
-    const mockSetShowTOS = jest.fn()
-    const mockSetShowPP = jest.fn()
+	const mockSetShowTOS = jest.fn()
+	const mockSetShowPP = jest.fn()
 
 	beforeEach(() => {
 		renderWithProviders(
 			<MemoryRouter>
 				<Footer 
-                    setShowTOS={mockSetShowTOS}
-                    setShowPP={mockSetShowPP}/>
+					setShowTOS={mockSetShowTOS}
+					setShowPP={mockSetShowPP}/>
 			</MemoryRouter>
 		)
 	})
 
-    test('Footer is rendered and displays correct info', () => {
-        expect(screen.getByText('Suomen Meriarkeologinen Seura ry')).not.toBeNull()
-        expect(screen.getByText('Finlands Marinarkeologiska Sällskap rf')).not.toBeNull()
-        expect(screen.getByText('Sähköposti:')).not.toBeNull()
-        expect(screen.getByText('Linkkejä')).not.toBeNull()
-        expect(screen.getByText('mas.fi')).not.toBeNull()
-        expect(screen.getByText('Yhteystiedot')).not.toBeNull()
-        expect(screen.getByText('MAS-portaali')).not.toBeNull()
-        expect(screen.getByText('Helsingin Yliopisto')).not.toBeNull()
-    })
+	test('Footer is rendered and displays correct info', () => {
+		expect(screen.getByText('Suomen Meriarkeologinen Seura ry')).not.toBeNull()
+		expect(screen.getByText('Finlands Marinarkeologiska Sällskap rf')).not.toBeNull()
+		expect(screen.getByText('Sähköposti:')).not.toBeNull()
+		expect(screen.getByText('Linkkejä')).not.toBeNull()
+		expect(screen.getByText('mas.fi')).not.toBeNull()
+		expect(screen.getByText('Yhteystiedot')).not.toBeNull()
+		expect(screen.getByText('MAS-portaali')).not.toBeNull()
+		expect(screen.getByText('Helsingin Yliopisto')).not.toBeNull()
+	})
 
-    test('Terms of Service can be clicked', async () => {
-        const user = userEvent.setup()
-        const tos = screen.getByTestId('tos')
-        await user.click(tos)
-    })
+	test('Terms of Service can be clicked', async () => {
+		const user = userEvent.setup()
+		const tos = screen.getByTestId('tos')
+		await user.click(tos)
+	})
 
-    test('Privacy policy can be clicked', async () => {
-        const user = userEvent.setup()
-        const pp = screen.getByTestId('pp')
-        await user.click(pp)
-    })
+	test('Privacy policy can be clicked', async () => {
+		const user = userEvent.setup()
+		const pp = screen.getByTestId('pp')
+		await user.click(pp)
+	})
 
 })
