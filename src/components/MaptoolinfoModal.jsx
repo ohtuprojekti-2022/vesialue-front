f3e09cf95497be87b4c8aa7529840d3033c8f383
Sippee <sippegaming@gmail.com>
2022-11-27 21:50:01 +0200

    fixed maptoolinfo

--- src/components/MaptoolinfoModal.jsx
+++ src/components/MaptoolinfoModal.jsx
@@ -11,23 +11,23 @@ const MaptoolinfoModal = ({ show, close }) => {
 				</Modal.Header>
 				<Modal.Body>
 					Hiirtä siirtäessä ylimmän vaihtoehdon päälle voidaan valita millaista karttaa käytetään tai halutaanko merimerkit näkyviin.
-					<div style={{display: 'flex', paddingLeft: '120px', paddingRight: '220px', paddingBottom:'20px'}}>
-						<img src='mti-menu.png' style={{border: '1px solid #000000', margin:'auto'}}></img>
-						<div style={{paddingLeft:'5px'}}>
-							<img src='mti-maps.png' style={{border: '1px solid #000000', margin:'auto'}}></img>
+					<div style={{display: 'flex', paddingLeft: '11%', paddingRight: '27%', paddingBottom:'4%'}}>
+						<img src='mti-menu.png' style={{border: '1px solid #000000', margin:'auto', maxWidth: '30%'}}></img>
+						<div style={{paddingLeft:'4%'}}>
+							<img src='mti-maps.png' style={{border: '1px solid #000000', margin:'auto', maxWidth: '100%'}}></img>
 						</div>
 					</div>
 					Viisikulmio-painike sallii uuden alueen piirtämisen kartalle.
 					Kartalle voi piirtää monimutkaisiakin alueita.
 					Alue on valmis, kun alku- ja loppupiste yhdistetään tai painetaan <q>Finish</q>-painiketta, 
 					jolloin se automaattisesti yhdistää alku- ja loppupisteen.
-					<div style={{display: 'flex', paddingBottom:'20px'}}>
+					<div style={{display: 'flex', paddingBottom:'4%'}}>
 						<img src='mti-create.png' style={{border: '1px solid #000000', margin:'auto', maxWidth: '100%'}}></img>
 					</div>
 					Piirrustus-painike sallii alueiden muokkauksen.
 					Alueita muokataan vetämällä neliöistä.
 					Muokkauksen tallennus onnistuu painamalla <q>Save</q>-painiketta			
-					<div style={{display: 'flex', paddingBottom:'20px'}}>
+					<div style={{display: 'flex', paddingBottom:'4%'}}>
 						<img src='mti-edit.png' style={{border: '1px solid #000000', margin:'auto', maxWidth: '100%'}}></img>
 					</div>
 					Alueiden poistaminen onnistuu roskakori-painikkeen avulla.
