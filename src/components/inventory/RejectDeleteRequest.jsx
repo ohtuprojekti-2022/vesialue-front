638d6b08310d2bc84b5c05b82f89b8618083367e
Jeemlei <eemeli.ingervo@gmail.com>
2022-11-30 12:53:13 +0200

    Redirect after rejecting deletion request

--- src/components/inventory/RejectDeleteRequest.jsx
+++ src/components/inventory/RejectDeleteRequest.jsx
@@ -3,25 +3,24 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { rejectDeletionById } from '../../services/inventory-service'
 import { removeDeletedInventoryById } from '../../redux/reducers/deletedInventoryReducer'
+import { useNavigate } from 'react-router-dom'
 
 const RejectDeletionButton = ({ id }) => {
 	const dispatch = useDispatch()
+	const navigate = useNavigate()
 	const handleClick = async () => {
 		try {
 			await rejectDeletionById(id)
 			dispatch(removeDeletedInventoryById(id))
-
+			navigate('/')
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
-	return(
-		<Button
-			variant="secondary"
-			onClick={handleClick}
-		>
-            Hylkää poistopyyntö
+	return (
+		<Button variant="secondary" onClick={handleClick}>
+			Hylkää poistopyyntö
 		</Button>
 	)
 }
