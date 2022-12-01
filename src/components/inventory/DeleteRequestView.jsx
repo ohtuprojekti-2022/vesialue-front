493032d293b84e245145eec3e04600e3cf4d9f74
Jeemlei <eemeli.ingervo@gmail.com>
2022-12-01 20:05:26 +0200

    Adjusted margins in DeleteRequestView

--- src/components/inventory/DeleteRequestView.jsx
+++ src/components/inventory/DeleteRequestView.jsx
@@ -6,25 +6,28 @@ import { Button } from 'react-bootstrap'
 
 const DeleteRequestView = ({ deleteRequest, isAdmin }) => {
 	const [showAdminModal, setShowAdminModal] = useState(false)
-	
+
 	return (
 		<>
-			<Alert variant='warning'>
-				<Alert.Heading>{isAdmin &&
-					'Tälle inventoinnille on tehty poistopyyntö'
-					|| 'Olet tehnyt tälle inventoinnille poistopyynnön'}</Alert.Heading>
+			<Alert variant="warning">
+				<Alert.Heading>
+					{(isAdmin && 'Tälle inventoinnille on tehty poistopyyntö') ||
+						'Olet tehnyt tälle inventoinnille poistopyynnön'}
+				</Alert.Heading>
 				<hr />
-				<p>
+				<p style={{ marginBottom: '0.5rem' }}>
 					Poiston syy: {deleteRequest.reason}
 				</p>
 				<RejectDeletionButton id={deleteRequest.id} />
-				{isAdmin && <Button
-					onClick={() => setShowAdminModal(true)}
-					variant='danger'
-					style={{ marginLeft: '0.5rem' }}
-				>
-					Hyväksy poistopyyntö
-				</Button>}
+				{isAdmin && (
+					<Button
+						onClick={() => setShowAdminModal(true)}
+						variant="danger"
+						style={{ marginLeft: '0.5rem', marginTop: '0.5rem' }}
+					>
+						Hyväksy poistopyyntö
+					</Button>
+				)}
 			</Alert>
 			<AdminDeleteModal
 				show={showAdminModal}
@@ -32,7 +35,6 @@ const DeleteRequestView = ({ deleteRequest, isAdmin }) => {
 				id={deleteRequest.inventory}
 			/>
 		</>
-
 	)
 }
 
