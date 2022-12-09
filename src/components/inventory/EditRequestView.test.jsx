722663115f8642cbf42ca4ac88c738cf52f65e7b
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-12-09 16:09:47 +0200

    add a test for editrequestview

--- src/components/inventory/EditRequestView.test.jsx
+++ src/components/inventory/EditRequestView.test.jsx
@@ -4,6 +4,7 @@ import EditRequestView from './EditRequestView'
 import React from 'react'
 import { screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
 const editRequest = {'id': '123'}
 
@@ -15,7 +16,7 @@ describe('EditRequestView', () => {
 
 		renderWithProviders(
 			<MemoryRouter>
-				<EditRequestView isAdmin={true}/>
+				<EditRequestView isAdmin={true} editRequest={editRequest}/>
 			</MemoryRouter>
 		)
 		alert = screen.getByTestId('edit-request-alert')
@@ -32,5 +33,11 @@ describe('EditRequestView', () => {
 
 	test('Renders text', () => {
 		expect(screen.getByText('Tälle inventoinnille on tehty muokkauspyyntö')).toBeVisible()
+	})
+
+	test('Button can be clicked', async () => {
+		const user = userEvent.setup()
+		const button = screen.getByRole('button', { name: /Tarkista pyyntö/i })
+		await user.click(button)
 	})
 })
