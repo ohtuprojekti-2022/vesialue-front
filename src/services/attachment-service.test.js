2d153646f827d47afe3c8fd531761c911f9a4138
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-12-15 11:04:30 +0200

    improve a test

--- src/services/attachment-service.test.js
+++ src/services/attachment-service.test.js
@@ -5,6 +5,19 @@ jest.mock('axios')
 
 describe('user-service', () => {
 
+    beforeEach(() => {
+        localStorage.setItem('userDetails', JSON.stringify({
+            auth: 'xyz',
+            user: {
+                id: 'abc',
+                name: '',
+                email: 'test@email.com',
+                phone: '',
+                username: 'testperson',
+            }}),
+        )
+    })
+
     test('uploadAttachment is called', async () => {
         try {
             const results = await uploadAttachment(
